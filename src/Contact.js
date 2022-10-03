cbb74f30c2e6444ecd1ec6903c6e0c528afb97c6
Lakshay Malhotra <lakshay.sbp@gmail.com>
2022-10-03 16:56:35 +0530

    l

--- src/Contact.js
+++ src/Contact.js
@@ -35,7 +35,51 @@ const Contact = () => {
 
   return (
     <Wrapper>
-      <h1>Contact page</h1>
+      <h2 className="common-heading">Contact page</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24067.051662431008!2d75.80580433625904!3d30.91058383370287!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a83c7eea8dd95%3A0x2fcef382bdff343!2sSilver%20Arc%20Mall!5e0!3m2!1sen!2sin!4v1664795308904!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullscreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form action="" method="POST" className="contact-inputs">
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+              value=""
+            />
+
+            <input
+              type="email"
+              placeholder="Email"
+              name="Email"
+              required
+              autoComplete="off"
+              value=""
+            />
+            <textarea
+              name="Messages"
+              placeholder="Enter Your Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+            />
+            <textarea />
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
     </Wrapper>
   );
 };
